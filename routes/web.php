ebd854a014517e7570bfb5d0fcee6ff398018acf
GodaiKobyashi <kobagody@gmail.com>
2021-11-07 08:56:36 +0900

    入力情報指摘

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/posts', 'PostController@index');
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
